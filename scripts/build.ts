51e76a1922763e9ae3d554680073ccd9219dda44
CodeCraft <72947633+codecraft3r@users.noreply.github.com>
2024-04-28 19:28:29 -0700

    make build.ts not care about locales

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,7 @@
 import { consola } from 'consola';
 import { cloneDeep, merge } from 'lodash-es';
 import { resolve } from 'node:path';
+import { existsSync } from 'node:fs';
 
 import { formatAndCheckSchema } from './check';
 import { config, localesDir, meta, plugins, pluginsDir, publicDir } from './const';
@@ -22,34 +23,43 @@ const build = async () => {
       const plugin = formatAndCheckSchema(data);
       if (!list[config.entryLocale]) list[config.entryLocale] = [];
       list[config.entryLocale].push(plugin);
-      for (const locale of config.outputLocales) {
-        if (!list[locale]) list[locale] = [];
-        const localeFilePath = resolve(localesDir, file.name.replace('.json', `.${locale}.json`));
-        const localeData = readJSON(localeFilePath);
-        list[locale].push(merge(cloneDeep(plugin), localeData));
+
+      if (existsSync(localesDir)) {
+        for (const locale of config.outputLocales) {
+          if (!list[locale]) list[locale] = [];
+          const localeFilePath = resolve(localesDir, file.name.replace('.json', `.${locale}.json`));
+          if (existsSync(localeFilePath)) {
+            const localeData = readJSON(localeFilePath);
+            list[locale].push(merge(cloneDeep(plugin), localeData));
+          } else {
+            list[locale].push(cloneDeep(plugin));
+          }
+        }
       }
     }
   }
 
   for (const locale of [config.entryLocale, ...config.outputLocales]) {
-    // @ts-ignore
-    pluginsIndex.plugins = list[locale].sort(
-      (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
-    );
+    if (list[locale]) {
+      // @ts-ignore
+      pluginsIndex.plugins = list[locale].sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+      );
 
-    let tags: string[] = [];
+      let tags: string[] = [];
 
-    pluginsIndex.plugins.forEach((plugin) => {
-      tags = [...tags, ...plugin.meta.tags];
-    });
+      pluginsIndex.plugins.forEach((plugin) => {
+        tags = [...tags, ...plugin.meta.tags];
+      });
 
-    tags = findDuplicates(tags);
+      tags = findDuplicates(tags);
 
-    pluginsIndex.tags = tags;
+      pluginsIndex.tags = tags;
 
-    const name = locale === config.entryLocale ? `index.json` : `index.${locale}.json`;
-    writeJSON(resolve(publicDir, name), pluginsIndex, false);
-    consola.success(`build ${name}`);
+      const name = locale === config.entryLocale ? `index.json` : `index.${locale}.json`;
+      writeJSON(resolve(publicDir, name), pluginsIndex, false);
+      consola.success(`build ${name}`);
+    }
   }
 };
 
